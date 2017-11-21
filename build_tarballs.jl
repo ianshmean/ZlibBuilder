52078f84185dddeea07f067349970a0645d8dc6a
Elliot Saba <staticfloat@gmail.com>
2017-11-20 16:00:15 -0800

    First draft of ZlibBuilder

---
+++ build_tarballs.jl
@@ -0,0 +1,43 @@
+using BinaryBuilder
+
+platforms = [
+  BinaryProvider.Linux(:i686, :glibc),
+  BinaryProvider.Linux(:x86_64, :glibc),
+  BinaryProvider.Linux(:aarch64, :glibc),
+  BinaryProvider.Linux(:armv7l, :glibc),
+  BinaryProvider.Linux(:powerpc64le, :glibc),
+  BinaryProvider.MacOS()
+]
+
+sources = [
+  "https://zlib.net/zlib-1.2.11.tar.gz" =>
+  "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1",
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir
+cd zlib-1.2.11/
+./configure --prefix=/
+make install -j3
+"""
+
+products = prefix -> [
+  LibraryProduct(prefix,"libz"),
+]
+
+autobuild(pwd(), "zlib", platforms, sources, script, products)
+
+
+# Special-case windows platforms
+platforms = [
+  BinaryProvider.Windows(:i686),
+  BinaryProvider.Windows(:x86_64),
+]
+script = raw"""
+cd $WORKSPACE/srcdir
+cd zlib-1.2.11/
+./configure --prefix=/
+make install SHAREDLIB=libz.dll SHAREDLIBM=libz-1.dll SHAREDLIBV=libz-1.2.11.dll LDSHAREDLIBC='' -j3
+"""
+autobuild(pwd(), "zlib", platforms, sources, script, products)
+
