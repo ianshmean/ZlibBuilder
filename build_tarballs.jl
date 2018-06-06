24989273eb8a5d35e462b612fa46b01b4fae3115
Alex Arslan <ararslan@comcast.net>
2018-06-06 16:30:35 -0700

    Build on all platforms

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -21,22 +21,12 @@ fi
 make install ${EXTRA_MAKE_FLAGS} -j${nproc}
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line.
-platforms = [
-    Windows(:i686),
-    Windows(:x86_64),
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc),
-    # Linux(:powerpc64le, :glibc),
-    MacOS()
-]
+# Build for ALL THE PLATFORMS!
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = prefix -> [
-    LibraryProduct(prefix,"libz", :libz),
+    LibraryProduct(prefix, "libz", :libz),
 ]
 
 # Dependencies that must be installed before this package can be built
